ed8032801e6b15751727cd20a9be007fee242b9a
Nazzcodek <bellnas09@gmail.com>
2023-09-10 09:33:01 +0100

    initial commit

---
+++ task_01.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def get_info() -> dict:
+    """
+    Get information from query parameters and return a JSON response.
+
+    Returns:
+        dict: A JSON response containing the retrieved information.
+    """
+    # Get query parameters
+    slack_name = request.args.get('slack_name')
+    track = request.args.get('track')
+
+    # Get current day and time
+    current_day = datetime.datetime.now().strftime('%A')
+    utc_time = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    # Define GitHub URLs
+    github_file_url = 'https://github.com/username/Nazzcodek/stage_one'
+    github_repo_url = 'https://github.com/Nazzcodek/stage_one'
+
+    # Create response dictionary
+    response = {
+        'slack_name': slack_name,
+        'current_day': current_day,
+        'utc_time': utc_time,
+        'track': track,
+        'github_file_url': github_file_url,
+        'github_repo_url': github_repo_url,
+        'status_code': 200
+    }
+
+    # Return JSON response
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
